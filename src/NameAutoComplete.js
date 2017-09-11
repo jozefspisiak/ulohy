7c9f66f085d384c265c90be3fb503dfc7c82286c
Jozef Spisiak <jozef.spisiak@vacuumlabs.com>
2017-09-11 17:50:57 +0200

    [WIP][TASK] Create nameAutoComplete element in react

--- src/NameAutoComplete.js
+++ src/NameAutoComplete.js
@@ -1,80 +1,191 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styles from './NameAutoComplete.css'
 
 class NameAutoComplete extends React.Component {
   constructor() {
     super();
-    this.emitChange = this.emitChange.bind(this);
-    this.state = {html: ''}
+    this.state = {text: [], names: [], suggested: 0, caret: 0}
   }
 
-  emitChange(e){
-    let element = ReactDOM.findDOMNode(this)
-    let html = element.innerHTML;
-    this.setState({html: html})
-    let name = this.findMatchingName(html)
+  emitChange(ev){
+    let element = ev.target
+    let inputText = element.textContent
     let caret = this.getCaretCharacterOffsetWithin(element)
-    console.log(caret)
-    ReactDOM.findDOMNode(this).innerHTML = name;
+
+
+    inputText = inputText.split(/[\s]+/)
+
+    // dealing with trailing spaces
+    //if (inputText[inputText.length - 1] === '')
+    //  inputText.pop()
+
+    /*let i = 0
+    for (let child of element.childNodes) {
+      //child.textContent = inputText[i]
+      //child.innerHtml += '&nbsp;'
+      //console.log(child);
+      if (child.nodeType === 3)
+        child.textContent = ''
+      i++
+    }
+    */
+
+    this.setState({text: inputText, caret: caret})
   }
 
-  findMatchingName(start){
-    let names = this.state.names
-    let match = names.indexOf(', ' + start)
-    if (match >= 0)
-      return names.substring(match+2,
-        match+2+names.substring(match+2).indexOf(','))
-    return start
+  handleClick(ev) {
+    const element = ev.target
+    const name = element.innerHTML
+    const caret = this.state.caret
+    let text = this.state.text
 
+    let currentWord = this.getCurrentWord()
+
+    const toAdd = name.substring(currentWord.length)
+
+    text[this.getCurrentWordIndex()] = name
+
+    this.setState({text: text, caret: caret+toAdd.length, suggested: 0})
   }
 
- getCaretCharacterOffsetWithin(element) {
-    var caretOffset = 0;
-    var doc = element.ownerDocument || element.document;
-    var win = doc.defaultView || doc.parentWindow;
-    var sel;
+  handleKeyPress(ev) {
+    let element = ev.target
+    console.log(element)
+    let key = ev.key
+    if (key === 'Enter') {
+
+    }
+  }
+
+  getCurrentWord() {
+    const text = this.state.text
+    let caret = this.state.caret
+
+    if (text.length === 0 || caret === 0 ) return ''
+
+    for (let name of text) {
+      if (caret <= name.length)
+        return name.substring(0,caret)
+      else
+        caret -= name.length+1
+    }
+
+    return ''
+  }
+
+  getCurrentWordIndex() {
+    const text = this.state.text
+    let caret = this.state.caret
+
+    if (text.length === 0 || caret === 0 ) return 0
+
+    let id = 0;
+    for (let word of text) {
+      if (caret <= word.length)
+        return id
+      else
+        caret -= word.length+1
+      id++
+    }
+
+    return text.length
+  }
+
+  findMatchingNames(){
+    const start = this.getCurrentWord()
+
+    if (start === '') return []
+    const names = this.state.names
+
+    let result = []
+    for (let id in names)
+      if (names[id].toLowerCase().indexOf(start.toLowerCase()) === 0)
+        if (names.hasOwnProperty(id)) {
+           result.push({id:id,name:names[id]})
+        }
+
+    return result
+  }
+
+  componentDidUpdate() {
+    let caret = this.state.caret
+    if (this.state.text.length !== 0 && caret !== 0) {
+      const element = ReactDOM.findDOMNode(this).firstChild;
+      let range = document.createRange()
+      let sel = window.getSelection()
+
+      // we need to clear user input because we surprassed warning
+      for (let child of element.childNodes) {
+        if (child.nodeType === 3)
+          child.textContent = ''
+
+        child.textContent = child.textContent.split(/[\s]+/)[0] + String.fromCharCode(160)
+      }
+
+      for (let child of element.childNodes) {
+        const text = child.textContent
+
+        if (caret <= text.length) {
+          let caretNode = child.firstChild
+          if (child.nodeType === 3) {
+            caretNode = child
+          }
+          range.setStart(caretNode, caret)
+          range.setEnd(caretNode, caret)
+          break;
+        } else {
+          if (child.textContent.length > 0)
+            caret -= child.textContent.length
+        }
+      }
+      sel.removeAllRanges();
+      sel.addRange(range);
+      element.focus();
+    }
+  }
+
+  // from https://stackoverflow.com/a/4812022
+  getCaretCharacterOffsetWithin(element) {
+    let caretOffset = 0
+    const doc = element.ownerDocument || element.document;
+    const win = doc.defaultView || doc.parentWindow;
+    let sel;
     if (typeof win.getSelection !== "undefined") {
         sel = win.getSelection();
         if (sel.rangeCount > 0) {
-            var range = win.getSelection().getRangeAt(0);
-            var preCaretRange = range.cloneRange();
+            let range = win.getSelection().getRangeAt(0);
+            let preCaretRange = range.cloneRange();
             preCaretRange.selectNodeContents(element);
             preCaretRange.setEnd(range.endContainer, range.endOffset);
             caretOffset = preCaretRange.toString().length;
         }
-    } else if ( (sel = doc.selection) && sel.type !== "Control") {
-        var textRange = sel.createRange();
-        var preCaretTextRange = doc.body.createTextRange();
+    } else {
+      sel = doc.selection
+      if ( sel.type !== "Control") {
+        let textRange = sel.createRange();
+        let preCaretTextRange = doc.body.createTextRange();
         preCaretTextRange.moveToElementText(element);
         preCaretTextRange.setEndPoint("EndToEnd", textRange);
         caretOffset = preCaretTextRange.text.length;
+      }
     }
     return caretOffset;
   }
 
   componentDidMount() {
 		this.readTextFile(this.props.txt);
-    var el = ReactDOM.findDOMNode(this);
-    var range = document.createRange();
-    var sel = window.getSelection();
-
-    range.setStart(el, el.innerHTML.length);
-    range.collapse(true);
-    sel.removeAllRanges();
-    sel.addRange(range);
-    el.focus();
 	}
 
-	readTextFile = file => {
-		var rawFile = new XMLHttpRequest();
-		rawFile.open("GET", file, false);
+	readTextFile(file) {
+		let rawFile = new XMLHttpRequest();
+		rawFile.open("GET", file, true);
 		rawFile.onreadystatechange = () => {
 			if (rawFile.readyState === 4) {
 				if (rawFile.status === 200 || rawFile.status === 0) {
-					var allNames = rawFile.responseText;
-
+          let names = rawFile.responseText.split(', ')
 					this.setState({
-						names: ', ' + allNames
+						names: names
 					});
 				}
 			}
@@ -82,11 +193,40 @@ class NameAutoComplete extends React.Component {
 		rawFile.send(null);
 	};
 
+  validateName(index) {
+    if (this.getCurrentWordIndex() === index)
+      return ''
+
+    const text = this.state.text
+    for (let name of this.state.names)
+      if (name.toLowerCase() === text[index].toLowerCase())
+        return 'nameautocomplete-valid'
+
+    return 'nameautocomplete-invalid'
+  }
+
   render() {
     return (
-      <div contentEditable
-      onInput={this.emitChange}
-      style={{border: 'solid 2px', width: 200+'px', height: 30+'px'}}/>
+      <div>
+        <div
+        contentEditable
+        suppressContentEditableWarning
+        onInput={this.emitChange.bind(this)}
+        className="nameautocomplete">
+          {this.state.text.map((name, index) =>
+            <span key={index} className={this.validateName(index)}>
+              {name + ' '}
+            </span>
+          )}
+        </div>
+        <ul>
+        {this.findMatchingNames().map((name) =>
+          <li key={name.id} onKeyPress={this.handleKeyPress.bind(this)} onClick={this.handleClick.bind(this)}>
+            {name.name}
+          </li>
+        )}
+        </ul>
+      </div>
     );
   }
 }
