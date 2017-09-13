945a39307ae1a9bf5136e48ef9b7aa67600ecb0a
Jozef Spisiak <jozef.spisiak@vacuumlabs.com>
2017-09-13 10:32:49 +0200

    [FIX] Simplified conditions if the promise array really contains promises

--- src/CustomPromise.js
+++ src/CustomPromise.js
@@ -10,21 +10,14 @@ CustomPromise.all = (promises) => {
     let pending = promises.length
     promises.forEach((promise,index) => {
       // this is needed in case promise array doesn't contain promises
-      if (Promise.resolve(promise) == promise)
-        promise.then((result) => {
-          results[index] = result
-          pending--
-          if (pending === 0)
-            resolveFunction(results)
-          },(error) =>
-          rejectFunction(error)
-        )
-      else {
-        results[index] = promise
+      Promise.resolve(promise).then((result) => {
+        results[index] = result
         pending--
         if (pending === 0)
           resolveFunction(results)
-      }
+        },(error) =>
+        rejectFunction(error)
+      )
     })
   })
 }
@@ -32,11 +25,7 @@ CustomPromise.all = (promises) => {
 CustomPromise.race = (promises) => {
   return new Promise((resolveFunction, rejectFunction) => {
     promises.forEach(promise => {
-      if (Promise.resolve(promise) == promise)
-        promise.then(resolveFunction).catch(rejectFunction)
-      else {
-        resolveFunction(promise)
-      }
+      Promise.resolve(promise).then(resolveFunction).catch(rejectFunction)
     });
   });
 }
