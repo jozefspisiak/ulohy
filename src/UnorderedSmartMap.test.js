eb37685c3573e5c2f67ac4b5b03192b7d6d31d1d
Jozef Spisiak <jozef.spisiak@vacuumlabs.com>
2017-09-14 16:48:29 +0200

    [TASK] add linter for tests

--- src/UnorderedSmartMap.test.js
+++ src/UnorderedSmartMap.test.js
@@ -1,25 +1,25 @@
-import UnorderedSmartMap from './UnorderedSmartMap';
+import UnorderedSmartMap from './UnorderedSmartMap'
 
 it('returns undefined', () => {
-    let sm = new UnorderedSmartMap();
-    sm.set([1, 2], 'foo')
-    sm.set([2, 3], 'bar')
-    sm.set([100000001, 11000001], 'Nanannananananannananannanana')
-    expect(sm.get([1, 3])).toEqual(undefined);
-    expect(sm.get([1, 1])).toEqual(undefined);
-    expect(sm.get([3, 3])).toEqual(undefined);
-    expect(sm.get([100000001, 11000000])).toEqual(undefined);
-});
+  const sm = new UnorderedSmartMap()
+  sm.set([1, 2], 'foo')
+  sm.set([2, 3], 'bar')
+  sm.set([100000001, 11000001], 'Nanannananananannananannanana')
+  expect(sm.get([1, 3])).toEqual(undefined)
+  expect(sm.get([1, 1])).toEqual(undefined)
+  expect(sm.get([3, 3])).toEqual(undefined)
+  expect(sm.get([100000001, 11000000])).toEqual(undefined)
+})
 
 it('returns correct value', () => {
-    let sm = new UnorderedSmartMap();
-    sm.set([1, 2], 'foo')
-    sm.set([2, 3], 'bar')
-    sm.set([100000001, 11000001], 'Nanannananananannananannanana')
-    sm.set([100000001, 11000001], 'Banana')
-    expect(sm.get([1, 2])).toEqual('foo');
-    expect(sm.get([2, 1])).toEqual('foo');
-    expect(sm.get([2, 3])).toEqual('bar');
-    expect(sm.get([3, 2])).toEqual('bar');
-    expect(sm.get([100000001, 11000001])).toEqual('Banana');
-});
+  const sm = new UnorderedSmartMap()
+  sm.set([1, 2], 'foo')
+  sm.set([2, 3], 'bar')
+  sm.set([100000001, 11000001], 'Nanannananananannananannanana')
+  sm.set([100000001, 11000001], 'Banana')
+  expect(sm.get([1, 2])).toEqual('foo')
+  expect(sm.get([2, 1])).toEqual('foo')
+  expect(sm.get([2, 3])).toEqual('bar')
+  expect(sm.get([3, 2])).toEqual('bar')
+  expect(sm.get([100000001, 11000001])).toEqual('Banana')
+})
